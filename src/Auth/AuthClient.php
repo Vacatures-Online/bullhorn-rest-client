d9e8cd2426573e4844e7ed4162aa165b86d11a0f
Sebastiaan <sebastiaan@Sebastiaans-MacBook-Pro.local>
2024-01-17 13:54:19 +0100

    Fix throw error

--- src/Auth/AuthClient.php
+++ src/Auth/AuthClient.php
@@ -303,7 +303,7 @@ class AuthClient implements AuthClientInterface {
         }
         catch (GuzzleException $e) {
             $errorMessage = $e->getMessage();
-            throw new BullhornAuthException("Failed to fetch authorization code (HTTP error: ${errorMessage})");
+            throw new BullhornAuthException('Failed to fetch authorization code (HTTP error: '.$errorMessage.')');
         }
     }
 }
